8a61d87cc768316941fc2b82fd5a9ce6916a6ead
Claudio Hernandez <claudioherza@gmail.com>
2024-06-07 00:45:40 -0400

    add header component

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          This is the new app header
+        </h1>
+
+      </header>
+      <p>
+        Editing  <code>src/app.js</code> and sve to reload
+      </p>
+    </div>
+  );
+}
+
+export default App;
